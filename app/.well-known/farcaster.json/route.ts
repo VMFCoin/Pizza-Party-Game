21a285a6d01d264ad90a77e2ba97146778990612
Kingkill666 <michael66gray@yahoo.com>
2025-11-24 22:16:52 -0800

    baseBuilder section added to Farcaster manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -38,6 +38,9 @@ const manifest = {
     header: "eyJmaWQiOjEwMTM0OTEsInR5cGUiOiJhdXRoIiwia2V5IjoiMHgyNTdDYmU4OTk2ODQ5NUMzYUU4QzgxQmNjQjhCRTdmMjU3Q0Q1ZjY2In0",
     payload: "eyJkb21haW4iOiJwaXp6YS1wYXJ0eS1nYW1lLnZtZmNvaW4uY29tIn0",
     signature: "V0PhSzCRcvgFOQjQ5+XvU+DKzsYBvpr3typ5jlkmb/IJSZQNQhxbCbmTYZsXs+cyw42a1wS2Cnxx4U610J8s4Rw="
+  },
+  baseBuilder: {
+    ownerAddress: "0x12e31f706010AE0996A2D8247c432d9102e3c871"
   }
 }
 
